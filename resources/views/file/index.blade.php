5826883ed528c643ff2218fcd7c89e76a127e645
Ignas <ignas.lunenas@mailerlite.com>
2019-04-02 13:24:41 +0300

    count added

--- resources/views/file/index.blade.php
+++ resources/views/file/index.blade.php
@@ -8,7 +8,7 @@
 
             <div class="col-12 d-flex justify-content-between align-items-center mb-4">
 
-                <h1 class="mb-0">Files</h1>
+                <h1 class="mb-0">Files {{ count($files) }}</h1>
 
                 <a class="btn btn-secondary" href="{{ route('file.create') }}">Add new file</a>
 
